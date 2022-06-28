25db2eb9a4c778f56a202b65640fde99a86dc9a2
Phutondog <ericmphu@gmail.com>
2022-06-28 17:29:00 -0400

    nav bar integration

--- Nutritio-Knights/src/app/common/header/nav-bar/nav-bar.component.ts
+++ Nutritio-Knights/src/app/common/header/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  toFight(){
+    this.router.navigateByUrl('fight')
+  }
+  toJournal() {
+    this.router.navigateByUrl('journal')
+  }
+
+  dummy(){
+    
   }
 
 }
